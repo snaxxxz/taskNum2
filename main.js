df9b90433c611a4bd4ca258d64320387154ef29b
Artur Adamyan <@r1666s>
2024-11-02 12:50:16 +0300

    feat: Создал первый блок СОздан первый с информацией о выбранном файле

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const header = document.createElement("div");
   header.classList.add('header');
   header.textContent = ("МОМЕНТЫ");
 
-
-
 const input = document.createElement('input');
   input.id = 'fileInput';
   input.type = 'file';
@@ -14,11 +12,44 @@ const input = document.createElement('input');
   input.setAttribute('accept','.png, .jpg, .jpeg');
   input.classList.add("input_file");
 
-
-
 const mainWind = document.createElement('div');
   mainWind.classList.add('main-block');
   mainWind.appendChild(input);
+
+const mainCardBlock = document.createElement('div');
+  mainCardBlock.classList.add('info-card__block');
+  mainWind.appendChild(mainCardBlock);
+
+  input.addEventListener('change', function(event) {
+    const files = event.target.files;
+
+    if (files.length > 5) {
+      alert('Можно  выбрать  не  более  5  файлов!');
+      return; 
+    }
+    for (const file of files) {
+      if (file.size > 10 * 1024 * 1024) {
+        alert(`Файл  "${file.name}"  слишком  большой!  Максимальный  размер  -  10  МБ.`);
+        return;
+      }
+    
+     const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+     if (!allowedTypes.includes(file.type)) {
+       alert(`Файл  "${file.name}"  не  является  JPEG  или  PNG!`);
+       input.value = ''; 
+       return; 
+      }
+      const fileInfo = document.createElement('div');
+      fileInfo.classList.add('info-card');
+        fileInfo.textContent = `
+        Имя: ${file.name}
+        Тип: ${file.type}
+        Размер: ${file.size / 1024} КБ
+        `;
+        mainCardBlock.appendChild(fileInfo);
+    }
+  });
+
 
 
   body.appendChild(header);
