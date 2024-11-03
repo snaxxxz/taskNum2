8699fa6c9fdae9fcea1fe85f47e1873639dbd86c
Artur Adamyan <@r1666s>
2024-11-03 22:31:02 +0300

    feat: создание дизайна Добавлены 2 фон. изображения стилизован блок Стилизована кнопка send

--- main.js
+++ main.js
@@ -1,68 +1,95 @@
 const body = document.body;
 
-const header = document.createElement("div");
-header.classList.add('header');
-header.textContent = ("МОМЕНТЫ");
+// Форма
+const myForm = document.createElement('form');
+myForm.id = 'inputForm';
+myForm.action = ' ';
+body.appendChild(myForm);
 
+// Инпут в форме
 const input = document.createElement('input');
-input.id = 'fileInput';
+input.id = 'myInput';
 input.type = 'file';
+input.name = '';
 input.setAttribute('multiple', true);
 input.setAttribute('accept', '.png, .jpg, .jpeg');
-input.classList.add("input_file");
+myForm.appendChild(input);
 
-const mainWind = document.createElement('div');
-mainWind.classList.add('main-block');
-mainWind.appendChild(input);
-
+// Див блок с файлами
 const mainCardBlock = document.createElement('div');
-mainCardBlock.classList.add('info-card__block');
-mainWind.appendChild(mainCardBlock);
+mainCardBlock.classList.add('main-card__block');
+myForm.appendChild(mainCardBlock);
 
 input.addEventListener('change', function(event) {
   const files = event.target.files;
 
+  // Кол-во выбранных файлов (макс знач: 5)
   if (files.length > 5) {
     alert('Можно  выбрать  не  более  5  файлов!');
-    return; 
+    return;
   }
+  // очищаем после выбранных
+  mainCardBlock.innerHTML = '';
+  // Размер одного файла не больше 10мб
   for (const file of files) {
-    if (file.size > 10 * 1024 * 1024) {
-      alert(`Файл  "${file.name}"  слишком  большой!  Максимальный  размер  -  10  МБ.`);
+    if (file.size > 10 * 1024 * 1024) { // 10 МБ = 10 * 1024 * 1024 байт
+      alert(`Файл "${file.name}" слишком большой!  Максимальный  размер  -  10  МБ.`);
       return;
     }
 
+    // Создаем массив для выбора файла
     const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
     if (!allowedTypes.includes(file.type)) {
-      alert(`Файл  "${file.name}"  не  является  JPEG  или  PNG!`);
-      input.value = ''; 
-      return; 
+      alert(`Файл "${file.name}" не  является  JPEG  или  PNG!`);
+      return;
     }
 
-    const fileInfo = document.createElement('div');
-    fileInfo.classList.add('info-card');
+    // Выводим информацию о файле в блок
+    const cardBlock = document.createElement('div'); // Создаем новый div для файла
+    cardBlock.classList.add('item-card');
 
+    // Создаем элемент img
     const previewImage = document.createElement('img');
     previewImage.classList.add('preview-image');
 
+    // Читаем файл как Base64 с помощью FileReader
     const reader = new FileReader();
     reader.onload = function(e) {
-      previewImage.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
+      previewImage.src = e.target.result; // Устанавливаем src для img
+    }
+    reader.readAsDataURL(file); // Читаем файл
 
-    fileInfo.textContent = `
-      Имя: ${file.name}
-      Тип: ${file.type}
-      Размер: ${file.size / 1024} КБ
-    `;
+    const textName = document.createElement('p');
+    cardBlock.appendChild(textName);
+    textName.textContent = `Имя: ${file.name}`;
+    
+    const textType = document.createElement('p');
+    cardBlock.appendChild(textType);
+    textType.textContent = `Тип: ${file.type}`;
 
-    fileInfo.appendChild(previewImage); 
+    const textSize = document.createElement('p');
+    cardBlock.appendChild(textSize);
+    textSize.textContent = `Размер: ${file.size} кб`;
 
-    mainCardBlock.appendChild(fileInfo);
+    // кнопка удаления блока
+    const btnRemove = document.createElement('button');
+    btnRemove.classList.add('btn-delete_block');
+    cardBlock.appendChild(btnRemove);
+    btnRemove.addEventListener ('click',function(){
+      mainCardBlock.removeChild(cardBlock);
+    });
+
+    // Добавляем img в div
+    cardBlock.appendChild(previewImage);
+    // Добавляем div в mainCardBlock
+    mainCardBlock.appendChild(cardBlock);
+    
   }
 });
 
-body.appendChild(header);
-body.appendChild(mainWind);
-  
+const btnSend = document.createElement('button');
+btnSend.classList.add('btn-sendForm');
+btnSend.type = 'submit';
+btnSend.innerHTML = 'SEND';
+myForm.appendChild(btnSend);
+
